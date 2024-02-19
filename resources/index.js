aa1e8838224d52a69a183af4584416b8ecb4fc1d
Bosif <boannarutjes@gmail.com>
2024-02-19 17:24:19 +0100

    added basic javascript

--- resources/index.js
+++ resources/index.js
@@ -1,1 +1,34 @@
+function updateTime() {
+  // Amsterdam
+  let amsterdamElement = document.querySelector("#amsterdam");
+  let amsterdamDateElement = amsterdamElement.querySelector(".date");
+  let amsterdamTimeElement = amsterdamElement.querySelector(".time");
+  let amsterdamTime = moment().tz("Europe/Amsterdam");
 
+  amsterdamDateElement.innerHTML = amsterdamTime.format("MMMM Do YYYY");
+  amsterdamTimeElement.innerHTML = amsterdamTime.format(
+    "h:mm:ss [<small>]A[</small]"
+  );
+
+  // Seoul
+  let seoulElement = document.querySelector("#seoul");
+  let seoulDateElement = seoulElement.querySelector(".date");
+  let seoulTimeElement = seoulElement.querySelector(".time");
+  let seoulTime = moment().tz("Asia/Seoul");
+
+  seoulDateElement.innerHTML = seoulTime.format("MMMM Do YYYY");
+  seoulTimeElement.innerHTML = seoulTime.format("h:mm:ss [<small>]A[</small]");
+
+  // Toronto
+  let torontoElement = document.querySelector("#toronto");
+  let torontoDateElement = torontoElement.querySelector(".date");
+  let torontoTimeElement = torontoElement.querySelector(".time");
+  let torontoTime = moment().tz("America/Toronto");
+
+  torontoDateElement.innerHTML = torontoTime.format("MMMM Do YYYY");
+  torontoTimeElement.innerHTML = torontoTime.format(
+    "h:mm:ss [<small>]A[</small]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
